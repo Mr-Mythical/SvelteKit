359711275dc2f6972e0e9c6ed62cd4b5cbe42742
Braüner <mathn519@gmail.com>
2025-09-25 07:57:38 +0200

    Develop (#62)
    * Updated score explanation status to updated for S3
    
    * linting
    
    * Converted all icons to webp and added missing icons
    
    * Removed file left over from icon download process
    
    * linting

--- src/lib/types/bossData.ts
+++ src/lib/types/bossData.ts
@@ -17,7 +17,8 @@ export const bosses: Boss[] = [
 			{ name: 'Obliteration Arcanocannon', id: 1219263 },
 			{ name: 'Eradicating Salvo', id: 1219531 },
 			{ name: 'Protocol: Purge', id: 1220489 },
-			{ name: 'Cleanse the Chamber', id: 1234733 }
+			{ name: 'Cleanse the Chamber', id: 1234733 },
+			{ name: 'Manifest Matrices', id: 1219450 }
 		]
 	},
 	{
@@ -44,7 +45,8 @@ export const bosses: Boss[] = [
 			{ name: 'Soulfire Convergence', id: 1225616 },
 			{ name: 'Soulfray Annihilation', id: 1227276 },
 			{ name: 'Mystic Lash', id: 1224025 },
-			{ name: 'Soul Calling', id: 1225582 }
+			{ name: 'Soul Calling', id: 1225582 },
+			{ name: 'Void Burst', id: 1227052 }
 		]
 	},
 	{
@@ -58,7 +60,8 @@ export const bosses: Boss[] = [
 			{ name: 'Invoke Collector', id: 1231720 },
 			{ name: 'Astral Harvest', id: 1228213 },
 			{ name: 'Void Tear', id: 1248171 },
-			{ name: 'Crushing Darkness', id: 1233074 }
+			{ name: 'Crushing Darkness', id: 1233074 },
+			{ name: 'Overwhelming Power', id: 1228502 }
 		]
 	},
 	{
@@ -78,7 +81,8 @@ export const bosses: Boss[] = [
 			{ name: 'Fel Devastation', id: 1227117 },
 			{ name: 'Aura of Pain', id: 1227154 },
 			{ name: 'Voidstep', id: 1227355 },
-			{ name: 'Encroaching Oblivion', id: 1235045 }
+			{ name: 'Encroaching Oblivion', id: 1235045 },
+			{ name: 'The Hunt', id: 1227809 }
 		]
 	},
 	{
@@ -94,11 +98,52 @@ export const bosses: Boss[] = [
 	{
 		id: 3134,
 		name: 'Nexus-King Salhadaar',
-		abilities: [{ name: 'TBD', id: 1 }]
+		abilities: [
+			{ name: 'Decree: Oath-Bound', id: 1224731 },
+			{ name: 'Dimension Glare', id: 1234539 },
+			{ name: "King's Hunger", id: 1228317 },
+			{ name: 'Subjugation Rule', id: 1224776 },
+			{ name: 'Command: Besiege', id: 1225016 },
+			{ name: 'Dimension Breath', id: 1228163 },
+			{ name: 'Banishment', id: 1227529 },
+			{ name: 'Command: Behead', id: 1225010 },
+			{ name: 'Reap', id: 1228053 },
+			{ name: 'Rally the Shadowguard', id: 1228065 },
+			{ name: 'Invoke the Oath', id: 1224906 },
+			{ name: 'Coalesce Voidwing', id: 1227734 },
+			{ name: 'Netherbreaker', id: 1228113 },
+			{ name: 'Cosmic Maw', id: 1234529 }
+		]
 	},
 	{
 		id: 3135,
 		name: 'Dimensius, the All-Devouring',
-		abilities: [{ name: 'TBD', id: 1 }]
+		abilities: [
+			{ name: 'Devour', id: 1229038 },
+			{ name: 'Massive Smash', id: 1230087 },
+			{ name: 'Dark Matter', id: 1230979 },
+			{ name: 'Fission', id: 1231005 },
+			{ name: 'Extinguish The Stars', id: 1231716 },
+			{ name: 'Gravity Well', id: 1232394 },
+			{ name: 'Supernova', id: 1232973 },
+			{ name: 'Supernova', id: 1232986 },
+			{ name: 'Unknown', id: 1234044 },
+			{ name: 'Shadowquake', id: 1234054 },
+			{ name: 'Inverse Gravity', id: 1234244 },
+			{ name: 'Cosmic Collapse', id: 1234263 },
+			{ name: 'Gamma Burst', id: 1237325 },
+			{ name: 'Mass Ejection', id: 1237694 },
+			{ name: 'Stardust Nova', id: 1237695 },
+			{ name: 'Extinction', id: 1238765 },
+			{ name: "Conqueror's Cross", id: 1239262 },
+			{ name: 'Unknown', id: 1243349 },
+			{ name: 'Reverse Gravity', id: 1243577 },
+			{ name: 'Shattered Space', id: 1243694 },
+			{ name: 'Antimatter', id: 1243702 },
+			{ name: 'Null Binding', id: 1246541 },
+			{ name: 'Mass Destruction', id: 1249423 },
+			{ name: 'Voidgrasp', id: 1250055 },
+			{ name: 'Starshard Nova', id: 1251619 }
+		]
 	}
 ];
